13caefdbf55c396e5d5985667c99f422e08093e3
Abhimanyu Khatry <abhimanyukhatry10@gmail.com>
2026-07-08 17:36:27 +0530

    Configure PostgreSQL support

--- sms/settings.py
+++ sms/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -36,8 +36,7 @@ ALLOWED_HOSTS = [
     ".onrender.com",
 ]
 
-#admin super username - abhimanyu
-#pass - abhi
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -87,14 +86,19 @@ WSGI_APPLICATION = 'sms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}"""
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
